ae4e979db287cb2828d2e8d9f6d4f6a4ae8c4a9a
Atharv <atharvbhandare2001@gmail.com>
2021-12-01 13:23:25 +0530

    Setting up the Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Pages
+import Create from "./pages/create/Create";
+import Home from "./pages/home/Home";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="create" element={<Create />}></Route>
+          <Route path="search" element={<Search />}></Route>
+          <Route path="recipe/:id" element={<Recipe />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
