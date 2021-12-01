21af8108c8d96b4f8b3d535efc1639c2f0d11bd7
Atharv <atharvbhandare2001@gmail.com>
2021-12-01 17:04:43 +0530

    Making a create recipe form in create.js

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,53 @@
+import { useState } from "react";
 import "./Create.css";
 
 export default function Create() {
-    return (
-        <div>
-            Create
-        </div>
-    )
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={submitHandler}>
+        <label>
+          <span>Recipe Title:</span>
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            value={title}
+            required
+          />
+        </label>
+        {/* ingredients goes here */}
+        {/* Method */}
+        <label>
+          <span>Cooking method:</span>
+          <input
+            onChange={(e) => setMethod(e.target.value)}
+            type="text"
+            value={method}
+            required
+          />
+        </label>
+        {/* cooking time */}
+        <label>
+          <span>Cooking Time (minutes):</span>
+          <input
+            onChange={(e) => setCookingTime(e.target.value)}
+            type="number"
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
 }
